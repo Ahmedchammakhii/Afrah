ddbde6841cddfa262bb3e603a669111384633c79
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-06-20 12:31:18 +0100

    hello

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import Hero from "./landing/hero"
+export default function Home() {
+  return (
+    <main >
+      <Hero />
+    </main>
+  )
+}
---
+++ data/hero.js
@@ -0,0 +1,3 @@
+export default [{
+    background: "red",
+}, { background: "green" }, { background: "yellow" }, { background: "black" }, { background: "pink" }]
