5506190c9511ba0a78abd7df6450d454c9417ad2
ChaabenFares <chaaben.fares94@gmail.com>
2023-07-01 15:19:42 +0200

    1st pull

--- functions/planning/2nd_section.js
+++ functions/planning/2nd_section.js
@@ -21,30 +21,45 @@ const Gallery = () => {
         alignItems: "center",
         flexDirection: "column",
         padding: 0,
-        left:0,position:"relative",
-        marginTop:3000,
-        //  paddingBottom:1000,
-        //  background:"transparent",
+        left:0,
+    //    zIndex:3
+       
            }}>
             <div
                 style={{
-                    height: '70%',
+                    height: '66.66%',
                     width: '100%',
                     backgroundImage: `url(${backgrounds[backgroundIndex]})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
-                    display: 'inline-block',
-                    
+                    // transform: "translateZ(-10px) scale(2) " ,
+                    // filter: "brightness(1.1) contrast(1.1)"   ,
+                    zIndex:3
                 }}
             ></div>
             <div style={{
-                height: '30%',
+                height: '33.33%',
                 width: '100%',
                 background: 'white',
-                display: 'inline-block',
-                
-                
-            }}></div>
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                zIndex:3,
+                flexDirection:"column"
+            }}>
+                <h2>Title</h2>
+                <div style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
+        gap: '16px',
+        marginTop: '16px',
+      }}>
+        <div style={{ backgroundImage: 'url("/1a.png")', height: '150px',width:"300px",backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
+        <div style={{ backgroundImage: 'url("/2a.jpg")', height: '150px',width:"300px" ,backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}></div>
+        <div style={{ backgroundImage: 'url("/3a.jpg")', height: '150px',width:"300px" ,backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}></div>
+      </div>
+            </div>
+             
         </div>
     );
 };
--- pages/form.js
+++ pages/form.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import fetch from 'isomorphic-unfetch';
 import ContactForm from '../functions/planning/ContactForm';
 import Gallery from '../functions/planning/2nd_section';
+import Image from 'next/image';
+import Fix from '../functions/planning/3rd_section';
+import CustomButton from '@/functions/planning/CustomButton';
 export default function Form() {
   const [message, setMessage] = useState('');
 
@@ -35,12 +38,16 @@ export default function Form() {
 
   return (
     <div style={{
-      display: 'flex',
+      display: "flex",
+      flexDirection: "column",
       justifyContent: 'center',
       alignItems: 'center',
       height: "100%",
-      width: "100%",
-      left: 0,top:0,position:"absolute"}} >
+      minWidth: "100vh",
+      left: 0,
+      top: 0,
+      // position:"relative"
+    }} >
       {/* <h1>Welcome to my Next.js app</h1>
       <button onClick={fetchData}>Fetch from Backend</button>
       {message && <p>{message}</p>} */}
@@ -48,42 +55,65 @@ export default function Form() {
 
       <ContactForm saveData={saveData} />
       <Gallery />
+      <Fix />
       <div style={{
-        display: 'flex',
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        left: 0,
+        bottom: 0,
+        background: 'linear-gradient( to bottom,  #ffffff,#d0e6e4)',
+        backgroundPosition: "center",
+        backgroundSize: 'cover',
+        zIndex: "2",
+        marginTop: -500,
+        display: "flex",
+        flexDirection: "column",
         justifyContent: 'center',
         alignItems: 'center',
-        height: "100%",
-        width: "100%",
-        left: 0,
-        // top:0,
-        backgroundImage: "url(/cupra.jpeg)",
-        backgroundPosition:"center",
-        backgroundSize:'cover',
-        backgroundRepeat:'no repeat',
-        zIndex: "-3",
-        position: "fixed",
-      
+
       }}
       >
+        <h2>THE BLOG</h2>
+        <div style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          gridTemplateRows: 'auto auto',
+          gap: '100px',
+          marginTop: '16px',
+          textAlign: "center",
+
+        }}>
+          <div style={{ backgroundImage: "url('/1b.jpg')", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat", height: '100%', width: "100%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}> </div>
+          <div style={{ backgroundImage: "url('/2b.jpg')", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat", height: "100%", width: "100%" }}></div>
+
+          <div style={{ backgroundImage: "url('/3b.jpg')", height: '500px', width: "400px", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
+        </div>
 
       </div>
       <div style={{
-        display: 'flex',
+        display: "flex",
+        height: "70vh",
+        width: "100vw",
+        left: 0,
+        bottom: 0,
+        background: 'white',
+        backgroundPosition: "center",
+        backgroundSize: 'cover',
+        zIndex: "2",
+
+        display: "flex",
+        flexDirection: "column",
         justifyContent: 'center',
         alignItems: 'center',
-        height: "100vh",
-        width: "100%",
-        left: 0,
-        // top:0,
-        background: "white",
-        backgroundPosition:"center",
-        backgroundSize:'cover',
-        zIndex: "3",
-        position: "absolute",
-      marginTop:6000
-      }}
-      >
 
+      }}>
+        <CustomButton 
+               label="YOU'RE FOLLOWING ON INSTAGRAM, RIGHT?"
+              isActive={false}
+              onClick={() => fileInputRef.current.click()}
+              />
+        {/* <button type="button" style={{ height: "60px", width: "300px", marginTop: "-300px", backgroundColor: "grey", borderRadius: "5rem" }}>Are you following us on instagram?</button> */}
       </div>
     </div>
   );
