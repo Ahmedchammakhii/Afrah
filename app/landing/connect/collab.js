c4129e44aa753a5497467fd0d72dfff61e8d5639
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-07-15 21:54:34 +0100

    fixed font style collab

--- app/landing/connect/collab.js
+++ app/landing/connect/collab.js
@@ -90,7 +90,7 @@ export default function Collab() {
                     .collab_bottom-paragraphe div { width: 50%; margin-left: 30%}
                     .collab_stop-press { margin-left: 2%;  padding: 6%; font-size: 3.5vw;  top: -25%;  }
                     .collab_right { width: 100%; position: relative; display: flex; justify-content: center; height: 88vw;; background: white }
-                    .collab_right-container {gap: 8%; width: 90% }
+                    .collab_right-container {gap: 8%; width: 90%; font-size: 3.7vw; text-transform: uppercase; font-weight: 500; font-style: normal}
                     .collab_right-links {  background: #f7f8fa; }
 
                 }
